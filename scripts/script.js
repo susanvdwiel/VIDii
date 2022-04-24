aebeb65d95afd1d9566e112a15cf00d294ae9fb1
susanvdwiel <74153185+susanvdwiel@users.noreply.github.com>
2022-04-24 20:46:17 +0200

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,2 +1,12 @@
-// JavaScript Document
-console.log("Howdy!");
+var deButton = document.querySelector(".beweeg");
+
+var deArticle = document.querySelector(".grijper");
+
+function beweegGrijper() {
+    deArticle.classList.toggle("grijper_beweeg");
+//    deArticle.classList.toggle("doei_woody");
+}
+
+deButton.addEventListener("click", beweegGrijper);
+//deButton.addEventListener("click"), beweegWoody);
+
